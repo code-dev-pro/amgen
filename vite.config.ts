a82e66fc04bcb4f78ce054716a3b9a04be6aa492
Tambouil <v.berceaux@gmail.com>
2024-06-17 20:27:55 +0200

    ci: fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/amgen/', // Ajoute cette ligne pour définir le chemin de base
+  base: './',
 });

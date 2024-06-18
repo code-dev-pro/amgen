dc9130f34d172cd0df2268e0009966fa8fa7848f
Tambouil <v.berceaux@gmail.com>
2024-06-18 11:12:13 +0200

    ci: fix base url

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,22 @@ import { Home } from './pages/Home';
 import { Menu } from './pages/Menu';
 import { ErrorPage } from './pages/ErrorPage';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Home />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: '/menu',
+      element: <Menu />,
+    },
+  ],
   {
-    path: '/',
-    element: <Home />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/menu',
-    element: <Menu />,
-  },
-]);
+    basename: '/amgen',
+  }
+);
 
 function App() {
   return <RouterProvider router={router} />;

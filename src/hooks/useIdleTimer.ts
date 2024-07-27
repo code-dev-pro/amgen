f2ba1cc06018d41104734cd79d867297207f286d
Tambouil <v.berceaux@gmail.com>
2024-07-27 19:08:44 +0200

    refactor(idle timer): better performances

--- src/hooks/useIdleTimer.ts
+++ src/hooks/useIdleTimer.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react';
+import { useState, useRef, useCallback, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Routes } from '../utils/routes';
 import { IDLE_TIMEOUT } from '../utils/variables';
@@ -8,19 +8,17 @@ export const useIdleTimer = (timeout = IDLE_TIMEOUT) => {
   const timerRef = useRef<number | null>(null);
   const navigate = useNavigate();
 
-  const resetTimer = () => {
-    if (timerRef.current) {
-      clearTimeout(timerRef.current);
-    }
-    timerRef.current = setTimeout(() => setIsIdle(true), timeout);
-  };
+  const resetTimer = useCallback(() => {
+    if (timerRef.current) clearTimeout(timerRef.current);
+    timerRef.current = window.setTimeout(() => setIsIdle(true), timeout);
+  }, [timeout]);
+
+  const handleActivity = useCallback(() => {
+    setIsIdle(false);
+    resetTimer();
+  }, [resetTimer]);
 
   useEffect(() => {
-    const handleActivity = () => {
-      setIsIdle(false);
-      resetTimer();
-    };
-
     window.addEventListener('mousemove', handleActivity);
     window.addEventListener('keydown', handleActivity);
     window.addEventListener('click', handleActivity);
@@ -32,17 +30,12 @@ export const useIdleTimer = (timeout = IDLE_TIMEOUT) => {
       window.removeEventListener('keydown', handleActivity);
       window.removeEventListener('click', handleActivity);
 
-      if (timerRef.current) {
-        clearTimeout(timerRef.current);
-      }
+      if (timerRef.current) clearTimeout(timerRef.current);
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [handleActivity, resetTimer]);
 
   useEffect(() => {
-    if (isIdle) {
-      navigate(Routes.Home);
-    }
+    if (isIdle) navigate(Routes.Home);
   }, [isIdle, navigate]);
 
   return { isIdle, resetTimer };

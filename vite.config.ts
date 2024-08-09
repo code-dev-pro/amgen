2c7ee8681dff693d8430e0a6acbd92624ec9c8e2
Tambouil <v.berceaux@gmail.com>
2024-08-09 12:08:07 +0200

    chore: setup pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,30 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'favicon.ico'],
+      manifest: {
+        name: "Explor'Amgen",
+        short_name: 'AMGEN',
+        description:
+          'Amgen est une application qui vous permet de répondre à un quiz sur différents sujets liés à Amgen.',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'favicon.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+          },
+        ],
+      },
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
